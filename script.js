29226417ec178cb53d165977212bc9c7ee9c3f86
yhkim <148407209+honeyprofit@users.noreply.github.com>
2025-04-08 22:22:52 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const btn = document.querySelector('.btn-country');
+const countriesContainer = document.querySelector('.countries');
+
+///////////////////////////////////////
+
+const request = new XMLHttpRequest();
+request.open('GET', 'https://restcountries.com/v2/name/portugal');
+request.send();
+// console.log(request.responseText);
+
+request.addEventListener('load', function () {
+  //   console.log(this.responseText);
+  const data = JSON.parse(this.responseText);
+  console.log(data);
+});
