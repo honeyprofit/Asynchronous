fd7a25b7d6f818b157cd19f616d556061f551479
yhkim <148407209+honeyprofit@users.noreply.github.com>
2025-04-19 22:17:25 +0900

    Update script.js

--- script.js
+++ script.js
@@ -5,6 +5,58 @@ const countriesContainer = document.querySelector('.countries');
 
 ///////////////////////////////////////
 
+const getCountryandNeighbour = function (country) {
+  // AJAX  all country 1
+  const request = new XMLHttpRequest();
+  request.open('GET', `https://restcountries.com/v2/name/${country}`);
+  request.send();
+
+  request.addEventListener('load', function () {
+    const [data] = JSON.parse(this.responseText);
+    console.log(data);
+
+    // render country 1
+    renderCountry(data);
+
+    // Get neighbour country (2)
+    const neighbour = data.borders?.[0];
+
+    if (!neighbour) return;
+
+    // AJAX  all country 2
+    const request2 = new XMLHttpRequest();
+    request2.open('GET', `https://restcountries.com/v2/alpha/${neighbour}`);
+    request2.send();
+
+    request2.addEventListener('load', function () {
+      const data2 = JSON.parse(this.responseText);
+      console.log(data2);
+
+      // render country 2
+      renderCountry(data2, 'neighbour');
+    });
+  });
+};
+
+const renderCountry = function (data, className = '') {
+  const html = `        
+  <article class="country ${className}">
+    <img class="country__img" src="${data.flag}" />
+    <div class="country__data">
+      <h3 class="country__name">${data.name}</h3>
+      <h4 class="country__region">${data.region}</h4>
+      <p class="country__row"><span>👫</span>${(
+        +data.population / 1000000
+      ).toFixed(1)}M people</p>
+      <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
+      <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
+    </div>
+  </article>
+  `;
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+  countriesContainer.style.opacity = 1;
+};
+
 const getCountryData = function (country) {
   const request = new XMLHttpRequest();
   request.open('GET', `https://restcountries.com/v2/name/${country}`);
@@ -35,6 +87,24 @@ const getCountryData = function (country) {
   });
 };
 
-getCountryData('portugal');
-getCountryData('USA');
-getCountryData('germany');
+// getCountryandNeighbour('portugal');
+// getCountryandNeighbour('USA');
+// getCountryandNeighbour('germany');
+
+// Callback hell
+setTimeout(() => {
+  console.log('1 second passed');
+  getCountryandNeighbour('portugal');
+  setTimeout(() => {
+    console.log('2 seconds passed');
+    getCountryandNeighbour('USA');
+    setTimeout(() => {
+      console.log('3 seconds passed');
+      getCountryandNeighbour('germany');
+      setTimeout(() => {
+        console.log('4 seconds passed');
+        getCountryandNeighbour('japan');
+      }, 1000);
+    }, 1000);
+  }, 1000);
+}, 1000);
